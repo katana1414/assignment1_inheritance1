b73284d14be66e611953db7fcaea59b2f93560fc
katana1414 <katanakerf@gmail.com>
2025-11-21 18:45:48 +0300

    Update №2 main.py

--- №2 main.py
+++ №2 main.py
@@ -1,14 +1,25 @@
-lecturer = Lecturer('Иван', 'Иванов')
-reviewer = Reviewer('Пётр', 'Петров')
-student = Student('Алёхина', 'Ольга', 'Ж')
- 
-student.courses_in_progress += ['Python', 'Java']
-lecturer.courses_attached += ['Python', 'C++']
-reviewer.courses_attached += ['Python', 'C++']
- 
-print(student.rate_lecture(lecturer, 'Python', 7))   # None
-print(student.rate_lecture(lecturer, 'Java', 8))     # Ошибка
-print(student.rate_lecture(lecturer, 'С++', 8))      # Ошибка
-print(student.rate_lecture(reviewer, 'Python', 6))   # Ошибка
- 
-print(lecturer.grades)  # {'Python': [7]}  
+from src.models.student import Student
+from src.models.lecturer import Lecturer
+from src.models.reviewer import Reviewer
+
+def main_demo():
+    # создание участников
+    student = Student('Алексей', 'Иванов')
+    lecturer = Lecturer('Иван', 'Иванов')
+    reviewer = Reviewer('Пётр', 'Петров')
+
+    # назначение курсов
+    student.courses_in_progress += ['Python', 'Java']
+    lecturer.courses_attached += ['Python', 'C++']
+    reviewer.courses_attached += ['Python', 'Java']
+
+    # сценарий использования
+    print(student.rate_lecture(lecturer, 'Python', 9))     # Оценка поставлена
+    print(student.rate_lecture(lecturer, 'Java', 8))       # Ошибка (Java не прикреплён лектору)
+    print(reviewer.rate_hw(student, 'Python', 8))          # Оценка за ДЗ поставлена
+    print(student)
+
+    print(lecturer)
+
+if __name__ == "__main__":
+    main_demo()
