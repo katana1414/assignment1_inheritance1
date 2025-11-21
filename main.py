32c9f9ea76c7ee5e23d36e74ba0e5cae141849ed
katana1414 <katanakerf@gmail.com>
2025-11-21 18:18:23 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+lecturer = Lecturer('Иван', 'Иванов')
+reviewer = Reviewer('Пётр', 'Петров')
+print(isinstance(lecturer, Mentor)) # True
+print(isinstance(reviewer, Mentor)) # True
+print(lecturer.courses_attached)    # []
+print(reviewer.courses_attached)    # []
