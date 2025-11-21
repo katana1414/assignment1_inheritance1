09551ccf61ef0a209f9567150753a972977079e3
katana1414 <katanakerf@gmail.com>
2025-11-21 18:34:59 +0300

    Create №2 main.py

---
+++ №2 main.py
@@ -0,0 +1,14 @@
+lecturer = Lecturer('Иван', 'Иванов')
+reviewer = Reviewer('Пётр', 'Петров')
+student = Student('Алёхина', 'Ольга', 'Ж')
+ 
+student.courses_in_progress += ['Python', 'Java']
+lecturer.courses_attached += ['Python', 'C++']
+reviewer.courses_attached += ['Python', 'C++']
+ 
+print(student.rate_lecture(lecturer, 'Python', 7))   # None
+print(student.rate_lecture(lecturer, 'Java', 8))     # Ошибка
+print(student.rate_lecture(lecturer, 'С++', 8))      # Ошибка
+print(student.rate_lecture(reviewer, 'Python', 6))   # Ошибка
+ 
+print(lecturer.grades)  # {'Python': [7]}  
