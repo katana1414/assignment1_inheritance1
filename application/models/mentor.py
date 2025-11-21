c824367a8b4652f162527cb40cffb0d9138789c9
katana1414 <katanakerf@gmail.com>
2025-11-21 18:21:18 +0300

    Create mentor.py

---
+++ application/models/mentor.py
@@ -0,0 +1,11 @@
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+class Lecturer(Mentor):
+    pass
+
+class Reviewer(Mentor):
+    pass
